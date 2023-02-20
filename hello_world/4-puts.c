fa8cc3eb20b4ca3c16d3ece761c1de2952b97d0b
HaroldS10 <haroldsanty13@gmail.com>
2023-02-20 12:04:17 -0800

    program finalized

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
- * Author: Harold in the file 4-puts.c
- **/
+* main - prints Author: Harold in the file 4-puts.c
+*
+* Return: always 0.
+**/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
