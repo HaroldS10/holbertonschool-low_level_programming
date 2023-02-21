25b6928bd9de49e468356b12770d0e2c82856b48
HaroldS10 <haroldsanty13@gmail.com>
2023-02-21 13:55:53 -0800

    las digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,7 +10,7 @@ int main(void)
         int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-printf("Last digit of %d\n is ", n);
+printf("Last digit of %d is ", n);
 n = abs(n) % 10;
 if (n > 5)
 {
