a84f97836afa9c6379a6f61892732e1ac9607a38
HaroldS10 <haroldsanty13@gmail.com>
2023-02-21 14:49:01 -0800

    alphabet/q_e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - alphabet except q and e
+* Return: Always 0.
+**/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c != 'q' && c != 'e')
+{
+putchar(c);
+}
+}
+putchar('\n');
+return (0);
+}
