48db46aef6d6957e2214a239992530866d559a44
HaroldS10 <haroldsanty13@gmail.com>
2023-02-21 16:38:28 -0800

    numbers of base 16 finished

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - numbers of base 16 in lowercase
+ * Return Always 0.
+ **/
+int main(void)
+{
+int h;
+
+for (h = 0; h < 16; h++)
+{
+if (h < 10)
+{
+putchar(h + '0');
+}
+else
+{
+putchar(h - 10 + 'a');
+}
+}
+putchar('\n');
+return (0);
+}
