aaff47ed08af978ab584ff730b4d5f175d0e7886
HaroldS10 <haroldsanty13@gmail.com>
2023-03-01 09:28:57 -0800

    even character completed

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <string.h>
-
+/**
+ * puts2 - prints every other character of a string, 
+ * starting with the first character, followed by a new line
+ * @str: Pointer to string
+ * return: 0
+ */
 void puts2(char *str)
 {
 int c;
