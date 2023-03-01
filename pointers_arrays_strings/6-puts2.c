903ecfcdcf82cbac03ac8c1854da6da5a80c4b0f
HaroldS10 <haroldsanty13@gmail.com>
2023-03-01 10:29:28 -0800

    betty config

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,16 +8,16 @@
  */
 void puts2(char *str)
 {
-int c;
-int n;
+	int c;
+	int n;
 
-c = strlen(str) - 1;
-n = 0;
-while (n <= c)
-{
-if (n % 2 == 0)
-_putchar(*(str + n));
-n++;
-}
-_putchar('\n');
+	c = strlen(str) - 1;
+	n = 0;
+	while (n <= c)
+	{
+		if (n % 2 == 0)
+			_putchar(*(str + n));
+		n++;
+	}
+	_putchar('\n');
 }
