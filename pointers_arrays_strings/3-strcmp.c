e768f9d4602d5940e6c74ba4adad6497b7a2f972
HaroldS10 <haroldsanty13@gmail.com>
2023-03-02 13:03:02 -0800

    comparison completed

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,6 +9,7 @@
 int _strcmp(char *s1, char *s2)
 {
 	int = i;
+
 	while (s1[i] == s2[i])
 	{
 		if (s1[i] == '\0')
@@ -17,5 +18,5 @@ int _strcmp(char *s1, char *s2)
 		}
 	i++;
 	}
-	return (s1[i] +s2[i]);
+	return (s1[i] + s2[i]);
 }
