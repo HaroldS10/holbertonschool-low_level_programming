647607805f54de74591ead36fa350686eedc4f2a
HaroldS10 <haroldsanty13@gmail.com>
2023-03-02 13:00:16 -0800

    Comparison completed

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - Compare two strings
+ * @s1: Pointer to first string
+ * @s2: Pointer to second string
+ * Return: The difference between the 2 first different characters in ASCII
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int = i;
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0')
+		{
+		return (0);
+		}
+	i++;
+	}
+	return (s1[i] +s2[i]);
+}
