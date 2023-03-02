11a460f188499a46b26cdceced83ac2a0f68d42b
HaroldS10 <haroldsanty13@gmail.com>
2023-03-02 08:41:41 -0800

    concatenate finished

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - Concatenates two strings
+ * @dest: Pointer to target string
+ * @src: Pointer to source string
+ * Return: pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int Cdest;
+	int Csrc;
+	int Cfdest;
+	int i;
+
+	Cdest = strlen(dest);
+	Csrc = strlen(src);
+	Cfdest = Cdest + Csrc;
+	i = 0;
+	while (Cdest <= Cfdest)
+	{
+		*(dest + Cdest) = *(src + i);
+		Cdest++;
+		i++;
+	}
+	return (dest);
+}
