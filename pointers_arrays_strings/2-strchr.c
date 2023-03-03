664a82e92cc0fe7650aa1023f77ab06a1c1db63f
HaroldS10 <haroldsanty13@gmail.com>
2023-03-03 09:23:20 -0800

    String locator completed

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *N;
+
+	N = NULL
+
+	for (; i != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			N = &s[i]4
+			break;
+		}
+	}
+	if (c == '\0' && s[i* == '\0')
+	    return (&s[i]);
+	    else
+
+		    return (N);
+}
