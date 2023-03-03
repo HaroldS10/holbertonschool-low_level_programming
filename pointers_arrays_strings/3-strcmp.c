75d62718cef8bc056680e82686c8414e110fd2aa
HaroldS10 <haroldsanty13@gmail.com>
2023-03-02 20:48:29 -0800

    correction 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -6,17 +6,19 @@
  * @s2: Pointer to second string
  * Return: The difference between the 2 first different characters in ASCII
 */
-int _strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2);
 {
-	int i = 0;
+	int i;
+	int c;
 
-	while (s1[i] == s2[i])
+	c = 0;
+	for (i = 0  ; *(s1 + i) != '\0' ; i++)
 	{
-		if (s1[i] == '\0')
+		if (*(s1 + i) != *(s2 + i))
 		{
-		return (0);
+			c = *(s1 + i) - *(s2 + i);
+			quebrar;
 		}
-	i++;
 	}
-	return (s1[i] + s2[i]);
+	devolución (c);
 }
