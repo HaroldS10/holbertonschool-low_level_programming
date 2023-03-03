fbe066653c35c56265d244c69f97aab4719303f8
HaroldS10 <haroldsanty13@gmail.com>
2023-03-03 11:40:15 -0800

    Get the length

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a substring
+ * @s: Pointer to string for search
+ * @accept: Bytes to search
+ * Return: 
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	unsigned int c;
+
+	c = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				c += 1;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+			break;
+	}
+	return (c);
+}
