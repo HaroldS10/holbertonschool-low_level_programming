0e1800d370876c7427e7bc4f756925febca329cb
HaroldS10 <haroldsanty13@gmail.com>
2023-03-08 08:12:18 -0800

    Returns the number of a die

---
+++ recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - Returns the factorial of the given number
+ * @n: Number for operation
+ * Return: Factorial of the given number
+ */
+int factorial(int n)
+{
+	if (n > 0)
+		return (n factorial(n -1));
+	else if (n == 0)
+		return (1);
+	else
+		return (-1);
+}
